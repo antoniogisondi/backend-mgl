474c5dc7b7362cd4b0b7596e93fece45ee25f022
Antonio Gisondi <anto.gisondi@tiscali.it>
2025-01-09 14:04:05 +0100

    fix generate certificate

--- utils/certificateGenerator.js
+++ utils/certificateGenerator.js
@@ -1,70 +1,14 @@
 const {PDFDocument, StandardFonts, rgb} = require('pdf-lib')
-const pdfParse = require('pdf-parse')
-const PizZip = require('pizzip')
-const DocxTemplater = require('docxtemplater')
 const fs = require('fs')
 const path = require('path')
 
-// exports.generateCertificate = async (participant, course) => {
-//         try {
-//             const templatePath = path.join(__dirname, '..', 'config', 'template', 'template.pdf')
-//             const pdfBuffer = fs.readFileSync(templatePath)
-//             const pdfDoc = await PDFDocument.load(pdfBuffer)
-//             const page = pdfDoc.getPages()
-//             const firstPage = page[0]
-
-//             firstPage.drawText(course.nome_corso, {
-//                 x: 120,
-//                 y:700,
-//                 size: 19,
-//                 color: rgb(0,0,0)
-//             })
-
-//             firstPage.drawText(`${participant.nome} ${participant.cognome}`, {
-//                 x: 120, // Coordinate per il segnaposto {NOME}
-//                 y: 680,
-//                 size: 12,
-//                 color: rgb(0, 0, 0),
-//             });
-    
-//             firstPage.drawText(participant.data_nascita, {
-//                 x: 120, // Coordinate per {DATA_DI_NASCITA}
-//                 y: 660,
-//                 size: 12,
-//                 color: rgb(0, 0, 0),
-//             });
-    
-//             firstPage.drawText(participant.comune_nascita, {
-//                 x: 120, // Coordinate per {CITTA_DI_NASCITA}
-//                 y: 640,
-//                 size: 12,
-//                 color: rgb(0, 0, 0),
-//             });
-    
-//             firstPage.drawText(participant.provincia_comune_nascita, {
-//                 x: 120, // Coordinate per {PROVINCIA_NASCITA}
-//                 y: 620,
-//                 size: 12,
-//                 color: rgb(0, 0, 0),
-//             });
-    
-//             firstPage.drawText(participant.mansione, {
-//                 x: 120, // Coordinate per {MANSIONE}
-//                 y: 600,
-//                 size: 12,
-//                 color: rgb(0, 0, 0),
-//             });
-
-//             const pdfBytes = await pdfDoc.save()
-//             fs.writeFileSync(`Attestato-${participant.nome}.pdf`, pdfBytes)
-//             return pdfBytes
-//         } catch (error) {
-//             console.error('Errore durante la generazione del certificato:', error);
-//             throw error;
-//         }
-// };
-
 exports.generateCertificate = async (participant,course) => {
+    const texts = {
+        data_di_nascita: `Nato/a il ${participant.data_nascita}, a ${participant.comune_nascita} (${participant.provincia_comune_nascita})`,
+        mansione: `Profilo professionale: ${participant.mansione}`,
+        descrizione_corso: `Ha frequentato il ${course.nome_corso} della durata di tot ore`
+    }
+
     try {
         const templatePath = path.join(__dirname, "..", "config", "template", "template.pdf");
         const pdfBuffer = fs.readFileSync(templatePath);
@@ -81,33 +25,49 @@ exports.generateCertificate = async (participant,course) => {
         const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
         const centerTextX = (text, fontSize) => {
-            const textWidth = font.widthOfTextAtSize(text, fontSize);
-            return (width - textWidth) / 2; // Centra il testo
+            const upperText = text.toUpperCase()
+            const textWidth = font.widthOfTextAtSize(upperText, fontSize);
+            return (width - textWidth) / 2 ; // Centra il testo
         };
 
-        const positions = {
-            nome_corso: height - 300,
-        }
-
         const replacements = [
-            { text: course.nome_corso, x: 180, y: 250, size: 19 },
-            { text: `${participant.nome} ${participant.cognome}`, x: 120, y: 680, size: 12 },
-            { text: participant.data_nascita, x: 120, y: 660, size: 12 },
-            { text: participant.comune_nascita, x: 120, y: 640, size: 12 },
-            { text: participant.provincia_comune_nascita, x: 120, y: 620, size: 12 },
+            { text: course.nome_corso.toUpperCase(), x: centerTextX(course.nome_corso, 19), y: 600, size: 19 },
+            { text: `${participant.nome.toUpperCase()} ${participant.cognome.toUpperCase()}`, x: centerTextX(`${participant.nome.toUpperCase()} ${participant.cognome.toUpperCase()}`, 12), y: 500, size: 12 },
+            { text: texts.data_di_nascita, x: 120, y: 660, size: 12 },
+            { text: texts.mansione, x: 120, y: 640, size: 12 },
+            { text: texts.descrizione_corso, x: 120, y: 620, size: 12 },
             { text: participant.mansione, x: 120, y: 600, size: 12 },
         ];
 
-          // Sovrascrivi i segnaposto visivamente
-        replacements.forEach(({ text, x, y, size }) => {
-        firstPage.drawText(text, {
-            x,
-            y,
-            size,
-            font,
-            color: rgb(0, 0, 0),
-        });
-        });
+        firstPage.drawText(replacements[0].text, {
+            x: replacements[0].x,
+            y: replacements[0].y,
+            size: replacements[0].size,
+        })
+
+        firstPage.drawText(replacements[1].text, {
+            x: replacements[1].x,
+            y: replacements[1].y,
+            size: replacements[1].size,
+        })
+
+        firstPage.drawText(replacements[2].text, {
+            x: replacements[2].x,
+            y: replacements[2].y,
+            size: replacements[2].size,
+        })
+
+        firstPage.drawText(replacements[3].text, {
+            x: replacements[3].x,
+            y: replacements[3].y,
+            size: replacements[3].size,
+        })
+
+        firstPage.drawText(replacements[4].text, {
+            x: replacements[4].x,
+            y: replacements[4].y,
+            size: replacements[4].size,
+        })
 
         const pdfBytes = await pdfDoc.save()
         return pdfBytes
